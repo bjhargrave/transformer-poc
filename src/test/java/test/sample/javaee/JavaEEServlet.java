fd76876d5464f7c849179fd20573063e62c2a641
Scott Marlow <smarlow@redhat.com>
2020-02-13 14:59:46 -0500

    javax.transaction.xa classes should not be transformed to jakarta

--- src/test/java/test/sample/javaee/JavaEEServlet.java
+++ src/test/java/test/sample/javaee/JavaEEServlet.java
@@ -7,10 +7,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.TransactionSynchronizationRegistry;
+import javax.transaction.xa.XAResource;
 
 public class JavaEEServlet extends HttpServlet implements Servlet {
 
 	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unused")
+	private XAResource xaResource;
+	@SuppressWarnings("unused")
+	private TransactionSynchronizationRegistry transactionSynchronizationRegistry;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
